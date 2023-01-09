b465abe9b94a87145e7798063294ce1d3498941e
Deeparti <deeparti93@gmail.com>
2023-01-09 13:09:54 +0530

    Chapter-01 initial commit

---
+++ App.js
@@ -0,0 +1,15 @@
+const heading1 = React.createElement("h1", { id: "title" }, "Heading 1");
+
+const heading2 = React.createElement("h2", { id: "title2" }, "Heading 2");
+
+const div = React.createElement(
+  "div",
+  {
+    id: "container",
+  },
+  [heading1, heading2]
+);
+
+const root = ReactDOM.createRoot(document.querySelector("#root"));
+
+root.render(div);
